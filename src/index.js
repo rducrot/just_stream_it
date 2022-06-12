07b193841a4cc1c4319e86aa1e0e9feccc8754c3
rducrot <ducrot.romain@gmail.com>
2022-06-12 21:32:09 +0200

    Fetch Best Movie

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import Movie from "./movie";
 
 let bestMovieContainer = document.getElementById("best-movie");
-let bestMovieTitle = document.createElement("p");
-bestMovieTitle.textContent = "Star Wars";
-bestMovieContainer.appendChild(bestMovieTitle);
-let moviesByScore = "";
+let bestMovieImage = document.getElementById("best-movie-img");
+let bestMovieTitle = document.getElementById("best-movie-title");
 fetch("http://localhost:8000/api/v1/titles/?format=json&sort_by=-imdb_score")
-                      .then(function(res){
-                        moviesByScore = res;
-                      });
-console.log(moviesByScore);
-
-function createCategories(){
-  
-}
+    .then(rest => rest.json())
+    .then(function(data){
+      let bestMovie = data.results[0];
+      bestMovieImage.src = bestMovie.image_url;
+      bestMovieImage.alt = bestMovie.title;
+      bestMovieTitle.textContent = bestMovie.title;
+    })
